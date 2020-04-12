c67dd987772b7d373ad0093b4a1c1d15fffea15c
Ilia Vakhitov <iv7180@gmail.com>
2020-04-12 17:50:43 +1000

    Added byrrons to main page

--- app/src/Components/Main/Main.js
+++ app/src/Components/Main/Main.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import { Component } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Button, Row, Col } from 'reactstrap';
+import { withRouter } from 'react-router-dom';
+
 import TestComponent from '../../TestComponent';
+
+import NewWord from '../../Components/Words/NewWord';
+import NewDictionary from '../../Components/Dictionaries/NewDictionary';
 
 class Main extends Component {
   
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newDictionary: false,
+      newWord: false  
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.onSaveDictionary = this.onSaveDictionary.bind(this);
+    this.onSaveWord = this.onSaveWord.bind(this);
+  }
+
+  handleClick(button) {
+    this.props.history.push('/'.concat([button.target.name]));
+  }
+
+  onSaveDictionary() {
+    // TODO
+    // show message
+    // TODO
+    this.props.history.push('/dictionaries');
+  }
+  
+  
+  onSaveWord() {
+    // TODO
+    // show message
+    // TODO
+    this.props.history.push('/words');
+  }
+  
+
   render() {
     const username = this.props.username;
     const welcomeString = username !== '' ? 'Welcome, ' + username + '!' : 'Welcome!';
@@ -13,9 +50,71 @@ class Main extends Component {
       <Container>
         <h3>{welcomeString}</h3>
         <TestComponent />
+        <br />
+        <Row xs='2'>
+          <Col>
+            <Button
+              className='mx-1 my-1'
+              name='games'
+              outline
+              color='dark'
+              onClick={this.handleClick}
+            >
+              Play revision game
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              className='mx-1 my-1'
+              name='profile'
+              outline
+              color='dark'
+              onClick={this.handleClick}
+            >
+              View profile
+            </Button>
+          </Col>          
+        </Row>
+        <Row xs='2'>
+          <Col>
+            <Button
+              className='mx-1 my-1'
+              name='dictionaries'
+              outline
+              color='dark'
+              onClick={this.handleClick}
+            >
+              View dictionaries
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              className='mx-1 my-1'
+              name='words'
+              outline
+              color='dark'
+              onClick={this.handleClick}
+            >
+              View words
+            </Button>
+          </Col>        
+        </Row>
+        <Row xs='2'>
+          <Col>
+            <NewDictionary 
+              onSaveDictionary={this.onSaveDictionary}
+            />
+          </Col>
+          <Col>
+            <NewWord 
+              dictionaryId={null} 
+              onSaveWord={this.onSaveWord}
+            />
+          </Col>
+        </Row>
       </Container>
     );
   }
 }
 
-export default Main;
+export default withRouter(Main);
