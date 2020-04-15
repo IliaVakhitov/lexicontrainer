ed68be58d08c3a137ae695087ea1886d28e7262e
Ilia Vakhitov <iv7180@gmail.com>
2020-04-15 20:56:49 +1000

    Rendering words in a potion of 5 items

--- app/src/Components/Words/Words.js
+++ app/src/Components/Words/Words.js
@@ -1,18 +1,77 @@
 import React from 'react';
 import { Component } from 'react';
 import { Container } from 'reactstrap';
-import { Card, CardHeader, CardBody,
-  CardTitle, CardText } from 'reactstrap'
+
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import Word from './Word';
 
 class Words extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {words: []};
-    this.all_words();
+    this.state = {
+      words: [],
+      wordsRender: [],
+      wordsRendered: 0,
+      dictionaries: [],
+      requestingData: false
+    };
+    
+    this._isMounted = false;
+
+    this.getWords = this.getWords.bind(this);
+    this.getDictionaries = this.getDictionaries.bind(this);
+    this.renderWords = this.renderWords.bind(this);
+
   }
 
-  all_words() {
+  componentDidMount() {
+    this._isMounted = true;
+    this._isMounted && this.getWords();
+    this._isMounted && this.getDictionaries();
+    this.myInterval = setInterval(() => {
+      this.renderWords();
+    }, 500);
+    // TODO 
+    // get dictiId from props
+    // get dict from props
+  }
+
+  getDictionaries() {
+    this.setState({
+      requestingData: true,
+    });
+
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('token'));  
+    fetch('/dicts/dictionaries_list', {
+      method: 'GET',
+      headers: myHeaders
+    })
+      .then(res => res.json())
+      .then(
+      (data) => {
+        if ('error' in data) {
+          console.log(data);
+          return;
+        }        
+        this.setState({
+          dictionaries: data.dictionaries,
+          requestingData: false
+        });
+      },
+      (error) => {
+        console.log(error);
+        this.setState({
+          requestingData: false
+        });
+      }
+    );  
+  }
+
+  getWords() {
+    
     var myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
     myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('token'));  
@@ -27,7 +86,11 @@ class Words extends Component {
           console.log(data);
           return;
         }
-        this.setState({words: data.words});
+        this.setState({ 
+          words: data.words
+        });
+        
+        
       },
       (error) => {
         console.log(error);
@@ -35,25 +98,49 @@ class Words extends Component {
     );   
   }
 
+  renderWords() {
+    let wordsRendered = this.state.wordsRendered;
+    if ( wordsRendered + 10 < this.state.words.length - 1) {
+      wordsRendered += 10
+    } else {
+      wordsRendered = this.state.words.length - 1;
+      clearInterval(this.myInterval);
+    }
+    this.setState({
+      wordsRender: this.state.words.slice(0, wordsRendered),
+      wordsRendered: wordsRendered
+    })
+  }
+
   render() {
-    const listItems = this.state.words.map(word => 
-      <Card key={word.id}>
-        <CardHeader tag="h3">{word.spelling}</CardHeader>
-        <CardBody>
-          <CardTitle>{word.definition}</CardTitle>
-          <CardText>Progress: {word.learning_index}</CardText>          
-        </CardBody>        
-      </Card>              
+    const listItems = this.state.wordsRender.map(word => 
+        <ListGroupItem 
+          key={word.id}
+          style={{borderStyle: 'none'}}          
+        >  
+          <Word    
+            dictionaries={this.state.dictionaries}   
+            updateList={this.getWords}
+            word={word} 
+          />   
+          
+        </ListGroupItem>  
+      
     );
     
     return (
+      
       <Container>
         <h3>All words</h3>
-        <ul>
+        <ListGroup>
           {listItems}          
-        </ul>
+        </ListGroup>
       </Container>
       );
   }
+  
 }
+
+
 export default Words;
+
